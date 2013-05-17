ab3ac2fc23475d5cc2f6a93067b25f5031ec0236
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-05-17 15:19:14 -0300

    Routes for list, favorites and publics added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Facere::Application.routes.draw do
   devise_for :users
   
+  resources :lists do
+    resources :tasks, :controller => 'lists/tasks'
+  end
+  
+  resources :favorites, :only => [:index, :create, :destroy]
+  resources :publics, :only => :index
+  
+  root :to => "home#index"
 end
