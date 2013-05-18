2e75eb899d28320d014fad82f85ceb8cb4cec8a2
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-05-18 12:59:39 -0300

    Correction on path for link to favorite

--- app/helpers/publics_helper.rb
+++ app/helpers/publics_helper.rb
@@ -6,11 +6,11 @@ module PublicsHelper
   
   #TODO REFATOR THIS  
   def switch_button(list)
-    favorite = Favorite.find_by_user_id_and_list_id(current_user.id, list)
-      if Favorite.user_list?(current_user, list)
-      link_to_favorite image_tag('favorite.png'), favorites_path(list), :method => :delete
+    favorite = Favorite.find_by_user_id_and_list_id(current_user, list)
+    if Favorite.user_list?(current_user, list)
+      link_to_favorite image_tag('favorite.png'), favorite_path(favorite, :list_id => list), :method => :delete
     else
-      link_to_favorite image_tag('nonfavorite.png'), favorites_path(favorite), :method => :post
+      link_to_favorite image_tag('nonfavorite.png'), favorites_path(:list_id => list), :method => :post
     end
   end
 end
