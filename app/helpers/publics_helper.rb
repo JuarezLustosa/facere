fd0cc92c214f4325d8217808e26139d19260d3d6
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-05-18 14:27:14 -0300

    Refactor, abstracted link to methods,  name changed to link_to_remote, more sense. Need  more refactor

--- app/helpers/publics_helper.rb
+++ app/helpers/publics_helper.rb
@@ -1,16 +1,30 @@
 module PublicsHelper
-  def link_to_favorite(content, path, options= {})    
+  def link_to_remote(content, path, options= {})    
     options.merge!(:remote => true )
     link_to content, path, options
   end
   
-  #TODO REFATOR THIS  
+  def remove_favorite(list, favorite)
+    link_to_remote(
+      image_tag('favorite.png'), 
+      favorite_path(favorite, :list_id => list), 
+      :method => :delete, 
+      :title => "Remove Favorite"
+    )
+  end
+  
+  def add_favorite(list)
+    link_to_remote(
+      image_tag('nonfavorite.png'), 
+      favorites_path(:list_id => list), 
+      :method => :post, 
+      :title => "Add Favorite"
+    )
+  end
+  
+  #TODO REFACTOR THIS  
   def switch_button(list)
-    favorite = Favorite.find_by_user_id_and_list_id(current_user, list)
-    if Favorite.user_list?(current_user, list)
-      link_to_favorite image_tag('favorite.png'), favorite_path(favorite, :list_id => list), :method => :delete
-    else
-      link_to_favorite image_tag('nonfavorite.png'), favorites_path(:list_id => list), :method => :post
-    end
+    favorite = Favorite.find_by_user_id_and_list_id(current_user, list)    
+    Favorite.user_list?(current_user, list) ? remove_favorite(favorite, list) : add_favorite(list)
   end
 end
