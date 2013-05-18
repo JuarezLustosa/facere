f3730f33317ff92cd0b53df5b6acc4f6aac14791
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-05-18 14:28:54 -0300

    Index of favorites filtered for logged users

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   respond_to :js
   
   def index
-    @favorites = Favorite.all
+    @favorites = current_user.favorites
   end
   
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  has_many :favorites
 end
