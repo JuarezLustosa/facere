539117f4d16a32f3c286997db6888c0956d17d5a
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-05-21 10:55:58 -0300

    English correction

--- spec/decorators/favorite_ability_spec.rb
+++ spec/decorators/favorite_ability_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe FavoriteAbility do
   let(:favorite) { stub(Favorite) }
-  let(:user) { stub(User)  }
+  let(:user) { stub(User) }
   let(:list) { stub(List) }
   
   context "#search" do
-    it "return the favorite list of user" do
+    it "return the favorite list" do
       Favorite.should_receive(:find_by_user_id_and_list_id).with(user, list)
       FavoriteAbility.new(user, list).search
     end
